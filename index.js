106066ba8b5785f6aa6b7bcae94299eb14165428
artemisCeres <89678876+artemisCeres@users.noreply.github.com>
2021-09-17 21:33:11 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
 'use strict';
-
-var express = require('express');
+//invoking the tools we are using (and some we are not)
+const http = require('http');
+const fs = require('fs');
+const ejs = require('ejs');
+const express = require('express');
+const app = express();
 const path = require('path');
-var app = express();
-var PORT = 3000;
 
-// With middleware
-app.use('/', function (req, res, next) {
+//initiate variable for storing ('name')
+let fname;
 
-    var options = {
-        root: path.join(__dirname)
-    };
-
-    const fileName = './static/home.html'
-    res.sendFile(fileName, options, function (err) {
-        if (!err) {
-            console.log('Sent:', fileName);
-        }
-
-        next(err);
-    });
-   
+//when browser is accessing localhost:3000 the server serves home.html
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './static/home.html'))
 });
 
-app.get('/', function (req, res) {
-    console.log("File Sent")
-    res.send();
+//when browser is accessing localhost:3000/'name' the server starts to look for ('name') and ('age')
+app.get('/:name', (req, res) =>{
+  let age = req.query.age;
+  fname  = req.params.name;
+
+//if there is no ('age') saved server sends age.html to ask for the ('age')
+if(!age){
+  res.sendFile(path.resolve(__dirname, './static/age.html'))
+}
+
+//if the age is younger than 18 server tells ('name') that they are too young
+else if(age < 18){
+  res.send(fname + ' you are too young to be here!')
+}
+
+//if name is of age server welcomes ('name')
+else{
+  res.send('Welcome ' + fname + '!')
+}
 });
 
-app.get('/:userName', function (req, res) {
-    const name = req.params.userName
-    const age = req.query.age
-
-    console.log(name, age)
-})
-
-app.listen(PORT, function (err) {
-    if (err) console.log(err);
-    console.log("Server listening on PORT", PORT);
+//the server is listening on port 3000 and allerts in the console in case it is accessed
+app.listen(3000, 'localhost', () => {
+  console.log('listening for requests on port 3000');
 });
